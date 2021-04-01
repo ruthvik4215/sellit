7183d8fcba7b0a2e2afe185537d56b984ea3a1a9
Ruthvik Bemidi <ruthvik4215@gmail.com>
2021-04-01 20:45:55 +0530

    created first react-native app.

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { PickerIOSComponent, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const name = "ruthvik";
+
+  if(name == "ruthvik") {
+    console.log("you are ",name);
+  } else {
+    console.log("you are not verified");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello my name is Ruthvik Bemidi</Text>
       <StatusBar style="auto" />
     </View>
   );
